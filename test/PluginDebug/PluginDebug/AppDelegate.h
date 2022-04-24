0da23afc371c4ff074d2a4e98eab46970daccea9
faimin <fuxianchao@gmail.com>
2022-04-24 17:12:25 +0800

    debug project

---
+++ test/PluginDebug/PluginDebug/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PluginDebug
+//
+//  Created by Zero.D.Saber on 2022/4/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test/PluginDebug/PluginDebugUITests/PluginDebugUITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  PluginDebugUITestsLaunchTests.m
+//  PluginDebugUITests
+//
+//  Created by Zero.D.Saber on 2022/4/24.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PluginDebugUITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation PluginDebugUITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
